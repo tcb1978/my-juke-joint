a9564ed7954f83a3122cdae99900ba8497673b46
Matthew Eldredge <mattheweldredge@gmail.com>
2017-12-28 12:16:24 -0700

    setup conroller.js with crud

--- server/controller.js
+++ server/controller.js
@@ -1,6 +1,38 @@
 require('dotenv').config();
 const axios = require('axios');
 
-module.exports = {
+let albums = [];
+let id = 0;
 
-}
+module.exports = {
+    create: (req, res) => {
+        const { track, artist } = req.body;
+        albums.push({ id, track, artist });
+        id++;
+        res.status(200).send(albums);
+    },
+
+    read: (req, res) => {
+        res.status(200).send(albums);
+    },
+
+    update: (req, res) => {
+        const updateID = req.params.id;
+        let index = albums.findIndex(album => album.id == updateID);
+
+        albums[index] = {
+            id: albums[index].id,
+            track: req.body.track || albums[index].track,
+            artist: req.body.artist || albums[index].artist
+        };
+
+        res.status(200).send(albums);
+    },
+
+    delete: (req, res) => {
+        const deleteID = req.params.id;
+        albumID = albums.findIndex(album => album.id == deleteID);
+        albums.splice(albumID, 1);
+        res.status(200).send(albums);
+    }
+};
--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ class App extends Component {
       
     }
     //bind
-    this.app = this.app.bind(this);
+    // this.app = this.app.bind(this);
   }
 
   render() {
